67b9d9a034ee283f77a964d896a86a6ab6a0d2c9
Rafal Bazan <rafalbe777@gmail.com>
2024-12-07 10:57:17 +0100

    completing instruction for serial config

--- microros_ws/README.md
+++ microros_ws/README.md
@@ -41,6 +41,47 @@ ros2 run micro_ros_setup build_firmware.sh
 ros2 run micro_ros_setup flash_firmware.sh
 ```
 
+#### for wifi version not serial
+
+
+[follow instruction in this tutorial](https://robofoundry.medium.com/esp32-micro-ros-actually-working-over-wifi-and-udp-transport-519a8ad52f65)
+```
+# 1. Create a directory for cloning the repo for ESP-IDF component
+mkdir ~/dev/esp32/microROS
+cd ~/dev/esp32/microROS
+
+# 2. Clone the ESP-IDF component repository
+git clone https://github.com/micro-ROS/micro_ros_espidf_component.git
+cd micro_ros_espidf_component
+
+# 3. Since we are going to be working with ROS2 Humble checkout humble branch
+git checkout -b humble 
+git status # to make sure we have the right branch
+
+# 4. Before you connect the ESP32 run following command in new terminal window
+sudo dmesg --follow
+
+# 5. connect the ESP32 to laptop while the command is running and it will show 
+# the port number the ESP32 is connected to - for me it was /dev/ttyUSB0
+# you can stop the command from running by hitting CTRL+C
+
+# 6. check the permission on that port by running
+ls -l /dev/ttyUSB0
+
+# it will show something like this, which means we don't have permissions 
+# to write to the port yet
+crw-rw---- 1 root dialout 188, 0 Aug 28 19:41 /dev/ttyUSB0
+
+# 7. Run following command to grant permission to write, we will need this
+# to flash ESP32 with the example program we are going to run which is
+# examples/int32_publisher
+sudo chmod 666 /dev/ttyUSB0
+
+docker run -it --rm --user espidf --volume="/etc/timezone:/etc/timezone:ro" -v  $(pwd):/micro_ros_espidf_component -v  /dev:/dev --privileged --workdir /micro_ros_espidf_component microros/esp-idf-microros:latest /bin/bash  -c "cd examples/int32_publisher; idf.py menuconfig build flash monitor"
+
+docker run -it --rm --net=host microros/micro-ros-agent:humble udp4 --port 8888 -v6
+```
+
 ### create and build micro-agent
 
 ```
--- microros_ws/blink/app.c
+++ microros_ws/blink/app.c
@@ -11,12 +11,8 @@
 #include "freertos/FreeRTOS.h"  // Add this include for FreeRTOS functions
 #include "freertos/task.h"      // Add this include for task-related functions
 #include <driver/gpio.h>
-#include <driver/ledc.h>
 
-#define LED_PIN_1 4    // Pin dla pierwszej diody LED
-#define LED_PIN_2 5    // Pin dla drugiej diody LED
-#define LED_PIN_3 18   // Pin dla trzeciej diody LED
-#define LED_PIN_4 19   // Pin dla czwartej diody LED
+#define LED_PIN 2
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
@@ -24,111 +20,25 @@
 rcl_subscription_t subscriber;
 std_msgs__msg__Int32 msg;
 
-bool fade_led = false; // Flaga do kontrolowania efektu płynnego zapalania LED
-
 void subscription_callback(const void * msgin)
 {
     const std_msgs__msg__Int32 * received_msg = (const std_msgs__msg__Int32 *)msgin;
     printf("Received: %d\n", received_msg->data);
 
-    // Ustaw flagę fade_led w zależności od parzystości liczby
+    // Zapal LED, gdy dane sa parzyste
     if (received_msg->data % 2 != 0) {
-        fade_led = true;
+        gpio_set_level(LED_PIN, 1); // Zapal LED
     } else {
-        fade_led = false;
-        // Wyłącz LED przez ustawienie wypełnienia PWM na 0 dla wszystkich kanałów
-        for (int i = 0; i < 4; i++) {
-            ledc_set_duty(LEDC_LOW_SPEED_MODE, i, 0);
-            ledc_update_duty(LEDC_LOW_SPEED_MODE, i);
-        }
-    }
-}
-
-void pwm_led_fade_task(void *arg) {
-    int duty = 0;
-    int fade_step = 30;   // Rozmiar kroku zmiany wypełnienia PWM
-    bool increasing = true;
-
-    while (1) {
-        if (fade_led) {
-            // Ustaw wypełnienie PWM dla wszystkich diod LED
-            for (int i = 0; i < 4; i++) {
-                ledc_set_duty(LEDC_LOW_SPEED_MODE, i, duty);
-                ledc_update_duty(LEDC_LOW_SPEED_MODE, i);
-            }
-
-            // Zmieniaj wypełnienie PWM dla płynnego efektu rozjaśniania i ściemniania
-            if (increasing) {
-                duty += fade_step;
-                if (duty >= 255) {
-                    duty = 255;
-                    increasing = false;
-                }
-            } else {
-                duty -= fade_step;
-                if (duty <= 0) {
-                    duty = 0;
-                    increasing = true;
-                }
-            }
-            vTaskDelay(pdMS_TO_TICKS(20));  // Czas między zmianami wypełnienia dla płynnego efektu
-        } else {
-            vTaskDelay(pdMS_TO_TICKS(100)); // Jeśli fade_led jest false, zadanie uśpione na dłużej
-        }
+        gpio_set_level(LED_PIN, 0); // Zgaś LED
     }
 }
 
 void appMain(void * arg)
 {
-    // Inicjalizacja PWM dla czterech diod LED
-    ledc_timer_config_t ledc_timer = {
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .timer_num = LEDC_TIMER_0,
-        .duty_resolution = LEDC_TIMER_8_BIT,
-        .freq_hz = 1000, // Częstotliwość PWM
-        .clk_cfg = LEDC_AUTO_CLK
-    };
-    ledc_timer_config(&ledc_timer);
-
-    ledc_channel_config_t ledc_channel_1 = {
-        .gpio_num = LED_PIN_1,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_0,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = 0,
-        .hpoint = 0
-    };
-    ledc_channel_config(&ledc_channel_1);
-
-    ledc_channel_config_t ledc_channel_2 = {
-        .gpio_num = LED_PIN_2,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_1,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = 0,
-        .hpoint = 0
-    };
-    ledc_channel_config(&ledc_channel_2);
-
-    ledc_channel_config_t ledc_channel_3 = {
-        .gpio_num = LED_PIN_3,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_2,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = 0,
-        .hpoint = 0
-    };
-    ledc_channel_config(&ledc_channel_3);
-
-    ledc_channel_config_t ledc_channel_4 = {
-        .gpio_num = LED_PIN_4,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .channel = LEDC_CHANNEL_3,
-        .timer_sel = LEDC_TIMER_0,
-        .duty = 0,
-        .hpoint = 0
-    };
-    ledc_channel_config(&ledc_channel_4);
+    // Inicjalizacja GPIO dla LED
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_PIN, 0); // Zgaś LED na początku
 
     rcl_allocator_t allocator = rcl_get_default_allocator();
     rclc_support_t support;
@@ -151,9 +61,6 @@ void appMain(void * arg)
     rclc_executor_t executor;
     RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
     RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
-
-    // Tworzenie zadania FreeRTOS do efektu płynnego zapalania LED
-    xTaskCreate(pwm_led_fade_task, "PWM_LED_Fade_Task", 2048, NULL, 1, NULL);
 
     while(1){
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
