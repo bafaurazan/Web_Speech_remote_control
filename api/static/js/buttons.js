91cfbc861022c0f0e3f7761a4201a3d1d534a66a
spyrtan <mateusz03b@gmail.com>
2024-12-11 20:52:51 +0200

    Added keyboard movement

--- api/static/js/buttons.js
+++ api/static/js/buttons.js
@@ -46,21 +46,87 @@ const roverbtnRight = document.getElementById('rover-btn-right');
 const roverbtnSpeedUp = document.getElementById('rover-btn-speed-up');
 
 // Przypisywanie funkcji do kliknięć
-dronebtnStart.addEventListener('click', () => console.log('Start'));
-dronebtnStop.addEventListener('click', () => console.log('Stop'));
-dronebtnForward.addEventListener('click', () => console.log('Przód'));
-dronebtnBackward.addEventListener('click', () => console.log('Tył'));
-dronebtnLeft.addEventListener('click', () => console.log('Lewo'));
-dronebtnRight.addEventListener('click', () => console.log('Prawo'));
-dronebtnUp.addEventListener('click', () => console.log('Do Góry'));
-dronebtnDown.addEventListener('click', () => console.log('Do Dołu'));
-dronebtnSpeedUp.addEventListener('click', () => console.log('Przyspiesz'));
+dronebtnStart.addEventListener('click', () => console.log('droneStart'));
+dronebtnStop.addEventListener('click', () => console.log('droneStop'));
+dronebtnForward.addEventListener('click', () => console.log('dronePrzód'));
+dronebtnBackward.addEventListener('click', () => console.log('droneTył'));
+dronebtnLeft.addEventListener('click', () => console.log('droneLewo'));
+dronebtnRight.addEventListener('click', () => console.log('dronePrawo'));
+dronebtnUp.addEventListener('click', () => console.log('droneDo Góry'));
+dronebtnDown.addEventListener('click', () => console.log('droneDo Dołu'));
+dronebtnSpeedUp.addEventListener('click', () => console.log('dronePrzyspiesz'));
 
-roverbtnStart.addEventListener('click', () => console.log('Start'));
-roverbtnStop.addEventListener('click', () => console.log('Stop'));
-roverbtnForward.addEventListener('click', () => console.log('Przód'));
-roverbtnBackward.addEventListener('click', () => console.log('Tył'));
-roverbtnLeft.addEventListener('click', () => console.log('Lewo'));
-roverbtnRight.addEventListener('click', () => console.log('Prawo'));
-roverbtnSpeedUp.addEventListener('click', () => console.log('Przyspiesz'));
+roverbtnStart.addEventListener('click', () => console.log('roverStart'));
+roverbtnStop.addEventListener('click', () => console.log('roverStop'));
+roverbtnForward.addEventListener('click', () => console.log('roverPrzód'));
+roverbtnBackward.addEventListener('click', () => console.log('roverTył'));
+roverbtnLeft.addEventListener('click', () => console.log('roverLewo'));
+roverbtnRight.addEventListener('click', () => console.log('roverPrawo'));
+roverbtnSpeedUp.addEventListener('click', () => console.log('roverPrzyspiesz'));
 
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase(); // Obsługa wielkich i małych liter
+
+    switch (key) {
+        case 'q':
+            document.getElementById('drone-btn-start').click();
+            break;
+        case 'w':
+            document.getElementById('drone-btn-forward').click();
+            break;
+        case 'e':
+            document.getElementById('drone-btn-stop').click();
+            break;
+        case 'a':
+            document.getElementById('drone-btn-left').click();
+            break;
+        case 's':
+            document.getElementById('drone-btn-backward').click();
+            break;
+        case 'd':
+            document.getElementById('drone-btn-right').click();
+            break;
+        case 'r':
+            document.getElementById('drone-btn-up').click();
+            break;
+        case 'f':
+            document.getElementById('drone-btn-down').click();
+            break;
+        default:
+            // Inne klawisze - brak akcji
+            break;
+    }
+});
+
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase(); // Obsługa wielkich i małych liter
+
+    // Mapowanie klawiszy dla panelu rover
+    switch (key) {
+        case 'q': // Start
+            document.getElementById('rover-btn-start').click();
+            break;
+        case 'w': // Przód
+            document.getElementById('rover-btn-forward').click();
+            break;
+        case 'e': // Stop
+            document.getElementById('rover-btn-stop').click();
+            break;
+        case 'a': // Lewo
+            document.getElementById('rover-btn-left').click();
+            break;
+        case 's': // Tył
+            document.getElementById('rover-btn-backward').click();
+            break;
+        case 'd': // Prawo
+            document.getElementById('rover-btn-right').click();
+            break;
+        case 'r': // Przyspiesz
+            document.getElementById('rover-btn-speed-up').click();
+            break;
+        default:
+            // Inne klawisze - brak akcji
+            break;
+    }
+});
